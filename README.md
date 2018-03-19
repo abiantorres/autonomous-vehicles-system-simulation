33886873b136bde48f5bd7e0e84ef1f78d723a8c
abiantorres <abiantorres.generic@gmail.com>
2018-03-19 23:17:45 +0000

    README.md changes

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # autonomous-vehicles-system-simulation
+
+## Dependencies:
+
+* For get rosjava working execute the followig command in a new terminal:
+
+```
+sudo apt-get install ros-kinetic-catkin ros-kinetic-rospack python-wstool openjdk-8-jdk
+```
---
+++ low_level_simulation/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/parallels/tfg/autonomous-vehicles-system-simulation/low_level_simulation/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/parallels/tfg/autonomous-vehicles-system-simulation/low_level_simulation/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/parallels/tfg/autonomous-vehicles-system-simulation/low_level_simulation/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/parallels/tfg/autonomous-vehicles-system-simulation/low_level_simulation/src:$ROS_PACKAGE_PATH"
